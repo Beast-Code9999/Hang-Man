21dd53f43c20b87529433e05b4f6d7c872bb38d4
Beast Code <beastcode9999@gmail.com>
2022-11-10 18:16:47 +0900

    add 1/3 of description of words

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 words = ['hair', 'passenger', 'library','poetry','category','revenue','analysis','magazine','height','economics','explanation','agency','preparation','birthday','diamond','wood','clothes','student','government','quality','elevator','speech','contract','disaster','fortune','classroom','leader','addition','supermarket','consequence','insect','outcome','leadership','dinner','chest','wedding','republic','mixture','interaction','advice']
 
+description = ['Black and thin, grows from folicles that are found on the dermis',
+    'a traveller on a public or private conveyance other than the driver, pilot, or crew.',
+    'a building or room containing collections of books',
+    'literary work in which the expression of feelings and ideas is given intensity by the use of distinctive style and rhythm',
+    'a class or division of people or things regarded as having particular shared characteristics.',
+    'income, especially when of an organization and of a substantial nature.',
+    'detailed examination of the elements or structure of something.',
+    'a periodical publication containing articles and illustrations',
+    'the measurement of someone or something from head to foot or from base to top.',
+    'the branch of knowledge concerned with the production, consumption, and transfer of wealth.',
+    'a statement or account that makes something clear.',
+    'a business or organization providing a particular service on behalf of another business, person, or group.',
+    ]
